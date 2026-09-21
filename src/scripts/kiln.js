062c392d302ed60c37af7759f33195038531cef0
Evelyn Dollar <dollarevelyn@gmail.com>
2026-09-21 14:01:55 -0500

    Fire pottery in the kiln

---
+++ src/scripts/kiln.js
@@ -0,0 +1,9 @@
+export const firePottery = (pottery, temperature) => {
+    pottery.fired = true
+    
+    if (temperature > 2200) {
+        pottery.cracked = true
+    } else { pottery.cracked = false
+    }
+    return pottery
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,31 @@
-// Imports go first
-import {makePottery} from "./PotteryWheel.js"
 
-// Make 5 pieces of pottery at the wheel
+import { makePottery } from "./PotteryWheel.js"
+
+import { firePottery } from "./kiln.js"
+
+
+
 let mug = makePottery("Mug", 3, 6)
 let bowl = makePottery("Bowl", 5, 4)
 let vase = makePottery("Vase", 7, 10)
 let plate = makePottery("Plate", 4, 2)
 let planter = makePottery("Planter", 8, 7)
 
-console.log(mug)
-console.log(bowl)
-console.log(vase)
-console.log(plate)
-console.log(planter)
 
 
 
-// Fire each piece of pottery in the kiln
+let firedMug = firePottery(mug, 2100)
+let firedBowl = firePottery(bowl, 2150)
+let firedVase = firePottery(vase, 2300)
+let firedPlate = firePottery(plate, 2200)
+let firedPlanter = firePottery(planter, 2050)
 
+
+console.log(firedMug)
+console.log(firedBowl)
+console.log(firedVase)
+console.log(firedPlate)
+console.log(firedPlanter)
 
 // Determine which ones should be sold, and their price
 
